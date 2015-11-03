5cc03409207764f4ac4feb8a35fbd835ed013eca
Jared Hensley <jahensle85@me.com>
2015-11-03 09:34:42 -0600

    toy 4 done

---
+++ toy4.js
@@ -0,0 +1,15 @@
+//write a function that returns the factorial of the number passed in as an argument 
+
+/*function factorial(n) {
+  
+}*/
+
+/*function fact(n) {
+
+  if (n > 0) {
+    return n * fact(n - 1);
+  } else {
+    return 1;
+  }
+
+}*/
